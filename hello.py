08121a73f0e24741daebf7be33991ba42c5561bc
99rstreet <2012rcj@gmail.com>
2017-03-30 13:07:55 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,19 @@
+import os
+import schedule  
+import time  
+ 
+ # Some jobs to execute later on:
+def job():
+	print "I am waiting....."  
+ #put the task to execute here  
+ 
+# def anotherJob():  
+ #another task can be defined here  
+ 
+ # schedule the first job to run every 10 minutes, the second for daily execution at 10.30am
+schedule.every(1).minutes.do(job)  
+ #schedule.every().day.at("10:30").do(anotherJob)  
+ 
+while True:  
+   schedule.run_pending()  
+   time.sleep(1) 
