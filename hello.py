56bf437a9a7a6bc884ecc894c3d837bcf6106439
99rstreet <2012rcj@gmail.com>
2017-03-31 11:43:31 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -4,10 +4,9 @@ import time
  
  # Some jobs to execute later on:
 #def job():
-	print "I am waiting....."  
+print "I am waiting....."  
  #put the task to execute here  
- 
-# def anotherJob():  
+ #def anotherJob():  
  #another task can be defined here  
 # schedule the first job to run every 10 minutes, the second for daily execution at 10.30am
 #schedule.every(1).minutes.do(job)  
